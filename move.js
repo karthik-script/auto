f0961f8567c19a734f0ad6082e534962269f7c0f
Kay <kay@Kays-MacBook-Pro.local>
2024-07-08 12:52:36 -0700

    5

--- move.js
+++ move.js
@@ -1,23 +1,40 @@
 // Move the mouse across the screen as a sine wave.
-var robot = require("robotjs");
+const robot = require("robotjs");
+const { getHours, getMinutes } = require("date-fns");
+const { utcToZonedTime } = require("date-fns-tz");
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function isTimeGreaterThan5PMPST() {
+  const timeZone = "America/Los_Angeles"; // PST/PDT time zone
+  const currentDate = new Date();
+  const zonedDate = utcToZonedTime(currentDate, timeZone);
+  const hours = getHours(zonedDate);
+  const minutes = getMinutes(zonedDate);
+
+  // Check if the current time in PST is greater than 5 PM
+  if (hours >= 17 && minutes >= 15) return true;
+  return false;
+}
 
 const init = async () => {
   // Speed up the mouse.
   robot.setMouseDelay(2);
 
-  var twoPI = Math.PI * 2.0;
-  var screenSize = robot.getScreenSize();
-  var height = screenSize.height / 2 - 10;
-  var width = screenSize.width;
+  const twoPI = Math.PI * 2.0;
+  const screenSize = robot.getScreenSize();
+  const height = screenSize.height / 2 - 10;
+  const width = screenSize.width;
   while (true) {
-    for (var x = 0; x < width; x++) {
-      y = height * Math.sin((twoPI * x) / width) + height;
+    for (let x = 0; x < width; x++) {
+      const y = height * Math.sin((twoPI * x) / width) + height;
       robot.moveMouse(x, y);
     }
     delay(5000);
     robot.keyTap("right_shift");
+    if (isTimeGreaterThan5PMPST()) {
+      return;
+    }
   }
 };
 
